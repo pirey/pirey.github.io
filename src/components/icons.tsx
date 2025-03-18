33fe8fb7a40f7613163e398e877c639ea3469de3
Yeri <mail@yeripratama.com>
2025-03-18 08:39:32 +0700

    fix: bootstrap logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -855,42 +855,45 @@ export function CoffeeScript({ className }: { className?: string }) {
 
 export function Bootstrap({ className }: { className?: string }) {
   return (
-    <svg viewBox="0 0 128 128" className={`size-4 ${className}`}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="512"
+      height="408"
+      viewBox="0 0 512 408"
+      className={`size-4 ${className}`}
+    >
       <defs>
         <linearGradient
-          id="a"
+          id="bs-logo-a"
           x1="76.079"
           x2="523.48"
           y1="10.798"
-          y2="365.95"
-          gradientTransform="translate(1.11 14.613) scale(.24566)"
+          y2="365.945"
           gradientUnits="userSpaceOnUse"
         >
-          <stop offset="0" stop-color="#9013fe" />
-          <stop offset="1" stop-color="#6610f2" />
+          <stop stopColor="#9013fe" />
+          <stop offset="1" stopColor="#6610f2" />
         </linearGradient>
         <linearGradient
-          id="b"
-          x1="193.51"
-          x2="293.51"
+          id="bs-logo-b"
+          x1="193.508"
+          x2="293.514"
           y1="109.74"
-          y2="278.87"
-          gradientTransform="translate(0 52)"
+          y2="278.872"
           gradientUnits="userSpaceOnUse"
         >
-          <stop offset="0" stop-color="#fff" />
-          <stop offset="1" stop-color="#f1e5fc" />
+          <stop stopColor="#fff" />
+          <stop offset="1" stopColor="#f1e5fc" />
         </linearGradient>
         <filter
-          id="c"
+          id="bs-logo-c"
           width="197"
           height="249"
-          x="161.9"
-          y="135.46"
-          color-interpolation-filters="sRGB"
+          x="161.901"
+          y="83.457"
           filterUnits="userSpaceOnUse"
         >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
           <feColorMatrix
             in="SourceAlpha"
             values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
@@ -903,14 +906,14 @@ export function Bootstrap({ className }: { className?: string }) {
         </filter>
       </defs>
       <path
-        fill="url(#a)"
-        d="M14.985 27.712c-.237-6.815 5.072-13.099 12.249-13.099h73.54c7.177 0 12.486 6.284 12.249 13.099-.228 6.546.068 15.026 2.202 21.94 2.141 6.936 5.751 11.319 11.664 11.883v6.387c-5.913.564-9.523 4.947-11.664 11.883-2.134 6.914-2.43 15.394-2.202 21.94.237 6.815-5.072 13.098-12.249 13.098h-73.54c-7.177 0-12.486-6.284-12.249-13.098.228-6.546-.068-15.026-2.203-21.94-2.14-6.935-5.76-11.319-11.673-11.883v-6.387c5.913-.563 9.533-4.947 11.673-11.883 2.135-6.914 2.43-15.394 2.203-21.94z"
+        fill="url(#bs-logo-a)"
+        d="M56.481 53.32C55.515 25.58 77.128 0 106.342 0h299.353c29.214 0 50.827 25.58 49.861 53.32-.928 26.647.277 61.165 8.964 89.31 8.715 28.232 23.411 46.077 47.48 48.37v26c-24.069 2.293-38.765 20.138-47.48 48.37-8.687 28.145-9.892 62.663-8.964 89.311.966 27.739-20.647 53.319-49.861 53.319H106.342c-29.214 0-50.827-25.58-49.86-53.319.927-26.648-.278-61.166-8.966-89.311C38.802 237.138 24.07 219.293 0 217v-26c24.069-2.293 38.802-20.138 47.516-48.37 8.688-28.145 9.893-62.663 8.965-89.31z"
       />
       <path
-        fill="url(#b)"
-        d="M267.1 364.46c47.297 0 75.798-23.158 75.798-61.355 0-28.873-20.336-49.776-50.532-53.085v-1.203c22.185-3.609 39.594-24.211 39.594-47.219 0-32.783-25.882-54.138-65.322-54.138h-88.74v217zm-54.692-189.48h45.911c24.958 0 39.131 11.128 39.131 31.279 0 21.505-16.484 33.535-46.372 33.535h-38.67zm0 161.96v-71.431h45.602c32.661 0 49.608 12.03 49.608 35.49 0 23.459-16.484 35.941-47.605 35.941z"
-        filter="url(#c)"
-        transform="translate(1.494 2.203) scale(.24566)"
+        fill="url(#bs-logo-b)"
+        filter="url(#bs-logo-c)"
+        stroke="#fff"
+        d="M267.103 312.457c47.297 0 75.798-23.158 75.798-61.355 0-28.873-20.336-49.776-50.532-53.085v-1.203c22.185-3.609 39.594-24.211 39.594-47.219 0-32.783-25.882-54.138-65.322-54.138h-88.74v217h89.202zm-54.692-189.48h45.911c24.958 0 39.131 11.128 39.131 31.279 0 21.505-16.484 33.535-46.372 33.535h-38.67v-64.814zm0 161.961v-71.431h45.602c32.661 0 49.608 12.03 49.608 35.49 0 23.459-16.484 35.941-47.605 35.941h-47.605z"
       />
     </svg>
   );
